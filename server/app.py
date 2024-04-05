7330c4b3ba262be28764b6607c0bb9c3e09505c7
dkiriinya <dkiriinya06sc@gmail.com>
2024-04-05 18:40:07 +0300

    Create an API with Flask for a React frontend application.

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -14,13 +15,82 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+           message_dict = message.to_dict()
+           messages.append(message_dict)
+        
+        response = make_response(
+            jsonify(messages),
+            200
+        )
+        
+        return response
+    
+    elif request.method == 'POST':
+        body = request.json.get("body")
+        username = request.json.get("username")
+        
+        if not body or not username:
+            return jsonify({"error": "Body and Username are required"}), 400
 
-@app.route('/messages/<int:id>')
+        new_message = Message(
+            body=body,
+            username=username,
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        message_dict = new_message.to_dict()
+        
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+        
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET','DELETE','PATCH'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    
+    if not message:
+         return jsonify({"error": "Message not found"}), 404
+     
+    if request.method == 'GET':
+        return jsonify(message.to_dict()), 200
+    
+    elif request.method == 'PATCH':
+        data = request.json
+        if 'body' in data:
+            message.body = data['body']
+        if 'username' in data:
+            message.username = data['username']
+        
+        db.session.commit()
+        
+        return jsonify(message.to_dict()), 200
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."
+        }
+
+        response = make_response(
+            jsonify(response_body),
+            200
+        )
+
+        return response
+     
 
 if __name__ == '__main__':
     app.run(port=5555)
